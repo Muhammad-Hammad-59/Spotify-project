42214f7006cddf6a9e33ae3e5efb9f414f66f2ba
Muhammad-Hammad-59 <muhammadhammad010100@gmail.com>
2024-02-16 07:14:19 +0500

    some modification in UI

---
+++ java.js
@@ -0,0 +1,12 @@
+let search=document.getElementById("search1");
+let searchbar=document.getElementsByClassName("searchBar");
+
+let check=1;
+function dis(){
+   
+        searchbar.classList.add("searchhide");
+    
+
+}
+
+search.addEventListener('click', dis());
